474311e97ae6bb94d0b90ef7c9f5bbcf52417ff7
Rajan Kashyap <rkashyap251@gmail.com>
2024-12-09 22:25:39 +0530

    adding images for how it works step

--- apps/frontend/components/ScrollSplitSection/FeatureCard.tsx
+++ apps/frontend/components/ScrollSplitSection/FeatureCard.tsx
@@ -1,7 +1,15 @@
 "use client";
+
 import classNames from "classnames";
+import Image from "next/image";
 
 import { useFeatureStore } from "./store";
+
+import FirstStep from "@/public/images/fistStep.webp";
+import SecondStep from "@/public/images/secondStep.webp";
+import ThirdStep from "@/public/images/thirdStep.webp";
+import FourthStep from "@/public/images/fourthStep.webp";
+import FifthStep from "@/public/images/fifthStep.webp";
 
 type FeatureCardProps = {
   gradient: string;
@@ -16,14 +24,28 @@ function FeatureCard({ children, gradient, id }: FeatureCardProps) {
   const isInView = useFeatureStore((state) => state.inViewFeature);
 
   return (
-    <div
-      className={classNames(
-        "absolute h-full w-full rounded-2xl bg-gradient-to-br opacity-0 transition-opacity",
-        gradient,
-        isInView === id ? "opacity-100" : "opacity-0"
-      )}
-    >
-      {children}
+    <div className="rounded-2xl">
+      <div
+        className={classNames(
+          "absolute z-10 h-full w-full rounded-[inherit] bg-gradient-to-br opacity-0 transition-opacity",
+          gradient,
+          isInView === id ? "opacity-100" : "opacity-0"
+        )}
+      >
+        {children}
+      </div>
+      {/* Todo: Create a moving border for the component */}
+      {/* <motion.div
+        className="absolute z-0 inset-0 rounded-2xl bg-gradient-to-br from-[#fff000] to-[#00ff00]"
+        initial={{ transform: "rotate(0deg)", opacity: 0 }}
+        animate={{ transform: "rotate(360deg)", opacity: 0 }}
+        transition={{
+          duration: 10,
+          repeat: Infinity,
+          repeatType: "loop",
+          ease: "linear",
+        }}
+      ></motion.div> */}
     </div>
   );
 }
@@ -31,7 +53,7 @@ function FeatureCard({ children, gradient, id }: FeatureCardProps) {
 export function RepositoryParse({ id }: CardProps) {
   return (
     <FeatureCard id={id} gradient="from-[#f7f0ff] to-[#a78afe]">
-      This is first step
+      <Image src={FirstStep} alt="" fill className="rounded-[inherit]" />
     </FeatureCard>
   );
 }
@@ -39,7 +61,7 @@ export function RepositoryParse({ id }: CardProps) {
 export function ReadmeLLM({ id }: CardProps) {
   return (
     <FeatureCard id={id} gradient="from-[#f5fbff] to-[#addeff]">
-      This is second step
+      <Image src={SecondStep} alt="" fill className="rounded-[inherit]" />
     </FeatureCard>
   );
 }
@@ -47,7 +69,7 @@ export function ReadmeLLM({ id }: CardProps) {
 export function EmbeddingsGenerate({ id }: CardProps) {
   return (
     <FeatureCard id={id} gradient="from-[#f5fff7] to-[#adf8ff]">
-      This is second step
+      <Image src={ThirdStep} alt="" fill className="rounded-[inherit]" />
     </FeatureCard>
   );
 }
@@ -55,7 +77,7 @@ export function EmbeddingsGenerate({ id }: CardProps) {
 export function UserQuestions({ id }: CardProps) {
   return (
     <FeatureCard id={id} gradient="from-[#f7fff5] to-[#adffd8]">
-      This is second step
+      <Image src={FourthStep} alt="" fill className="rounded-[inherit]" />
     </FeatureCard>
   );
 }
@@ -63,7 +85,7 @@ export function UserQuestions({ id }: CardProps) {
 export function GetAnswer({ id }: CardProps) {
   return (
     <FeatureCard id={id} gradient="from-[#fff7f5] to-[#ffd8ad]">
-      This is second step
+      <Image src={FifthStep} alt="" fill className="rounded-[inherit]" />
     </FeatureCard>
   );
 }
