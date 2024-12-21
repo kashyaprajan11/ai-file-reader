f4b2ab5433cdbdd2a07f44f87a48515b6bea224b
Rajan Kashyap <rkashyap251@gmail.com>
2024-12-21 07:06:20 +0530

    Removing the features section for now

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         </p>
       </WavyBackground>
       <ScrollSplitSection />
-      <Features />
+      {/* <Features /> */}
       <UseCases />
       <TechUsed />
       <GetInTouch />
