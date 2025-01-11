81b06880febd2d8cb8d4ce4f2972a1722014ef0d
Rajan Kashyap <rkashyap251@gmail.com>
2025-01-12 00:14:43 +0530

    Fixing the Tech Used section

--- apps/frontend/components/TechUsed/index.tsx
+++ apps/frontend/components/TechUsed/index.tsx
@@ -1,39 +1,40 @@
 "use client";
 
 import * as React from "react";
-
 import { IconCloud } from "../IconCloud";
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const slugs = [
+  "typescript",
+  "javascript",
+  "react",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "amazonaws",
+  "serverless",
+  "postgresql",
+  "nginx",
+  "vercel",
+  "git",
+  "github",
+  "visualstudiocode",
+  "babel",
+  "tailwindcss",
+  "figma",
+];
 
 export default function TechUsed() {
-  const targetRef = React.useRef();
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  );
 
   return (
     <div>
-      <div>
+      <div className="flex flex-col justify-center items-center">
         <p className="text-center text-6xl mb-10">Tech Used</p>
-        <IconCloud
-          iconSlugs={[
-            "react",
-            "typescript",
-            "javascript",
-            "nextdotjs",
-            "nodejs",
-            "tailwindcss",
-            "serverless",
-            "postgresql",
-            "git",
-            "github",
-            "vercel",
-            "amazonaws",
-            "redux",
-            "webpack",
-            "babel",
-            "awslambda",
-            "express",
-          ]}
-        />
+        <IconCloud images={images} />
       </div>
     </div>
   );
